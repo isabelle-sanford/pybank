04f3e400de713e0de8b37a4cd1c6129a6b664ed0
Isabelle Sanford <isabellesanford@gmail.com>
2020-09-22 13:21:37 -0700

    Update to new methods from class

--- hw.py
+++ hw.py
@@ -1,35 +1,26 @@
 import csv
 
-# open file and extract data in array
-file1 = open("budget_data.csv", "r")
-lines = []
+# open file and extract data in list
+with open("budget_data.csv", "r") as f:
+    budgetdata = csv.reader(f)
+    lines = list(budgetdata)
+#print(lines[0])
 
-for x in file1:
-    lines.append(x)
+# split each line into month & num
+lists = list(zip(*lines))
+months = lists[0][1:]
+profits = lists[1][1:]
 
-file1.close()
-
-# # split each line into month & num, take out '\n'
-for i in range(len(lines)):
-    pair = lines[i].split(",")
-    if len(pair) > 1:
-        pair[1] = pair[1][:-1]
-
-    lines[i] = pair
-
-
-
-# split up lists for easier use
-months = []
-profits = []
-for j in range(1,len(lines)):
-    months.append(lines[j][0])
-    profits.append(int(lines[j][1]))
+def prev(value, list1):
+    index = list1.index(value)
+    return list1[index - 1]
 
 # list of change month-by-month
-net_change = []
-for k in range(1, len(profits)):
-    net_change.append(profits[k] - profits[k-1])
+# net_change = []
+# for k in range(1, len(profits)):
+#     net_change.append(profits[k] - profits[k-1])
+
+net_change = [(k - prev(k, profits)) for k in profits]
 
 
 # Variables we want
@@ -37,16 +28,23 @@ num_months = len(lines) # what about if empty spaces?
 net_total = sum(profits)
 avg_change = sum(net_change) / len(net_change) # mean function? numpy?
 
-max_increase = net_change.index(max(net_change))
-max_decrease = net_change.index(min(net_change)) # need to translate these back to lines1 pair
+max_increase = lines[net_change.index(max(net_change)) + 1]
+max_decrease = lines[net_change.index(min(net_change)) + 1] 
 
-max_increase_pair = lines[max_increase + 1] # according to answers should be +2. Why?
-#print(max_increase_pair)
+# max_increase_pair = lines[max_increase + 1] # according to answers should be +2. Why?
+# #print(max_increase_pair)
 
-final_output = "Financial Analysis: \n" 
-final_output += "Total Months: " + str(num_months) + "\n"
-final_output += "Total: " + str(net_total) + "\n"
-final_output += "Average Change: " + str(avg_change) + "\n"
-final_output += "Max min stuff"
+print(f"Financial Analysis: ",
+        "Total Months: {num_months}", 
+        "Total: {net_total}",
+        "Average Change: {avg_change}",
+        "Biggest Increase: {max_increase}",
+        "Biggest Decrease: {max_decrease}")
 
-print(final_output)
+# final_output = "Financial Analysis: \n" 
+# final_output += "Total Months: " + str(num_months) + "\n"
+# final_output += "Total: " + str(net_total) + "\n"
+# final_output += "Average Change: " + str(avg_change) + "\n"
+# final_output += "Biggest increase: "
+
+# print(final_output)
