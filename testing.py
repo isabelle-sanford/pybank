ebcbcaf9bd0c2679e24498d92dc4bc79eee9872c
Isabelle Sanford <isabellesanford@gmail.com>
2020-09-23 12:22:41 -0700

    Fix average change variable

--- testing.py
+++ testing.py
@@ -16,10 +16,11 @@ profits = [int(x) for x in lists[1]]
 
 net_change = [int(profits[k] - profits[k - 1]) for k in range(1, len(profits))]
 
-# Variables we want
-num_months = len(lines) # what about if empty spaces?
-net_total = sum(profits)
-avg_change = sum(net_change) / len(net_change) # mean function? numpy?
+avg_change = sum(net_change) / len(net_change) 
+
+
+
+
 
 max_increase = lines[net_change.index(max(net_change))]
 max_decrease = lines[net_change.index(min(net_change))] 
@@ -28,7 +29,7 @@ max_decrease = lines[net_change.index(min(net_change))]
 final_output = ("Financial Analysis: \n" +
         f"Total Months: {num_months}\n" +
         f"Total: ${net_total}\n" +
-        f"Average Change: ${round(avg_change, 2)}\n" +
+        f"Average Change: ${avg_change}\n" +
         f"Biggest Increase: {max_increase[0]} (${max(net_change)})\n" +
         f"Biggest Decrease: {max_decrease[0]} (${min(net_change)})")
 
@@ -40,5 +41,7 @@ print(final_output)
 with open("analysis.txt", "w", newline = "") as analysis:
     analysis.write(final_output)
 
+print(net_total / 86)
 
-
+# change_change = [(net_change[p] - net_change[p-1]) for p in net_change]
+# print(change_change)
